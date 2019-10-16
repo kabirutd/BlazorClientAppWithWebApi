0630b94b24e27a320c926c6fef3e0db5fcf6a28d
Mohammad Kabir <mohammadkabir@gmail.com>
2019-10-15 23:36:38 -0500

    10162019

---
+++ ApplicationCore/Entities/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationCore.Entities
+{
+    public class Student
+    {
+
+        public string StudentId { get; set; }
+        //[Required]
+        public string FirstName { get; set; }
+        //[Required]
+        public string LastName { get; set; }
+        // [Required]
+        public string School { get; set; }
+    }
+}
---
+++ Infrastructure/Data/StudentRepository.cs
@@ -0,0 +1,99 @@
+﻿using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Runtime.InteropServices;
+using Dapper;
+
+namespace Infrastructure.Data
+{
+    public class StudentRepository: IAsyncRepository<Student>
+    {
+        public Task<Student> AddAsync(Student entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> CountAsync(ISpecification<Student> spec)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(Student entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        //Task<Student> IAsyncRepository<Student>.GetByIdAsync(int id)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        public async Task<Student> GetByIdAsync(int id)
+        {
+            var vdbconnectionstring = "";
+            var sqlquery = "";
+            var dbconnectionstring = "";
+            string vsqlquery = " ";
+
+            if (sqlquery != "" && sqlquery != null)
+            {
+
+                vsqlquery = sqlquery;
+
+            }
+
+            if (dbconnectionstring != "" && dbconnectionstring != null)
+            {
+
+                vdbconnectionstring = dbconnectionstring;
+
+            }
+
+            else
+            {
+                //To Do: Throw no connection error
+                //vdbconnectionstring = DataUtility.GetDBConnectionString(site, devenvironment);
+            }
+
+            SqlConnection dbConnection = new SqlConnection(vdbconnectionstring);
+
+            using (IDbConnection conn = dbConnection)
+
+            {
+                //TO DO - Get rid of VendorQualityPerformanceRepository.cs since it's doign the same
+                conn.Open();
+                var result = await conn.QueryAsync<Student>(vsqlquery, commandTimeout: conn.ConnectionTimeout);
+                return result.ToList();
+
+            }
+        }
+
+        public Task<IReadOnlyList<Student>> ListAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<Student>> ListAsync(ISpecification<Student> spec)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Student entity)
+        {
+            throw new NotImplementedException();
+        }
+
+      
+
+        //Task<Student> IAsyncRepository<Student>.GetByIdAsync(int id)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
