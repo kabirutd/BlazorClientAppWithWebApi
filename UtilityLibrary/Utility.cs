819068fa077b76f3a7ae7e76075b852d54def158
Mohammad Kabir <mohammadkabir@gmail.com>
2019-10-15 21:15:26 -0500

    10152019

---
+++ UtilityLibrary/Utility.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UtilityLibrary
+{
+    public class Utility
+    {
+
+        public static DateTime GetFirstDayOfWeek(DateTime dateToCheck, string firstDay)
+        {
+            DateTime dt = new DateTime();
+            for (int k = 0; k < 7; k++)
+            {
+                if (dateToCheck.AddDays(-k).DayOfWeek.ToString() == firstDay)
+                {
+                    dt = dateToCheck.AddDays(-k);
+                }
+            }
+            return dt;
+        }
+
+        public static DateTime GetLastDayOfWeek(DateTime dateToCheck, string firstDay)
+        {
+            DateTime dt = new DateTime();
+            for (int k = 0; k < 7; k++)
+            {
+                if (dateToCheck.AddDays(k).DayOfWeek.ToString() == firstDay)
+                {
+                    dt = dateToCheck.AddDays(k);
+                }
+            }
+            return dt;
+        }
+    }
+}
